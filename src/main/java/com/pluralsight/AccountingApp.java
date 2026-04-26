5cc6c830d7880b203fe513a8dd69c4d2beec547f
ramenmac <kevinnguyen4200@gmail.com>
2026-04-26 02:13:56 -0500

    Add Deposit and Payment functionalities and logs into file

--- src/main/java/com/pluralsight/AccountingApp.java
+++ src/main/java/com/pluralsight/AccountingApp.java
@@ -1,9 +1,14 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Formatter;
 import java.util.Scanner;
 
 public class AccountingApp {
@@ -16,13 +21,16 @@ public class AccountingApp {
         boolean isRunning = true, inLedger;
         String userChoice, ledgerChoice;
 
+        // Read data from .csv and load into an ArrayList
         loadData();
 
+        // Continuously runs program until user wants to exit
         do{
+            // Displays home menu and prompt user for menu selection
+            displayHomeMenu();
             userChoice = input.nextLine().toLowerCase();
 
-            displayHomeMenu();
-
+            // Switch case for menu option and invalid input for default
             switch (userChoice){
                 case "d":
                     addDeposit();
@@ -64,7 +72,6 @@ public class AccountingApp {
                 default:
                     println("Invalid Option... TRY AGAIN >:(");
             }
-
         }while(isRunning);
     }
     static void println(String message){
@@ -73,6 +80,8 @@ public class AccountingApp {
     static void loadData(){
 
         try{
+
+            // Opens and reads files
             FileReader fileReader = new FileReader("star_stream_transactions.csv");
             BufferedReader bufRead = new BufferedReader(fileReader);
             bufRead.readLine(); // Clears the header
@@ -80,6 +89,7 @@ public class AccountingApp {
             String[] parsedData;
 
             while(line != null){
+                // Split the string and add it into transactions arraylist
                 parsedData = line.split("\\|");
                 transactionList.add(new Transaction(parsedData[0], parsedData[1], parsedData[2], parsedData[3],Double.parseDouble(parsedData[4])));
                 line = bufRead.readLine();
@@ -88,10 +98,94 @@ public class AccountingApp {
             println("File could not be found");
         }
     }
-    static void displayHomeMenu(){}
+    static void displayHomeMenu(){
 
-    static void addDeposit(){}
-    static void makePayment(){}
+        // Basic menu for now
+        println("""
+                ------------------------------------------------------------------------------------------
+                
+                                                        Menu Options
+                                                   (D)  Make Deposit
+                                                   (P)  Make Payment
+                                                   (L)     Ledger
+                                                   (X)      Exit
+                
+                ------------------------------------------------------------------------------------------""");
+    }
+
+    static void addDeposit(){
+
+        // Add deposit into Arraylist
+        Transaction userDeposit = promptDeposit();
+        transactionList.add(userDeposit);
+
+        // Write into the file
+        logTransaction(userDeposit);
+    }
+    static Transaction promptDeposit(){
+
+        // Format for date / time
+        DateTimeFormatter logDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter logTimeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        // Fetch current date time and prompt user for deposit details
+        ZonedDateTime depositTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
+
+        println("What is the reason for the deposit?");
+        String description = input.nextLine().trim();
+        println("What is your constellation modifier?");
+        String vendor = input.nextLine().trim();
+        println("How many coins would like to deposit?");
+        double amount = input.nextDouble();
+        input.nextLine(); // Clears buffer
+
+        Transaction userDeposit = new Transaction(depositTime.format(logDateFormat), depositTime.format(logTimeFormat), description, vendor, amount);
+        return userDeposit;
+    }
+    static void makePayment(){
+
+        // Add Payment into Arraylist
+        Transaction userPayment = promptPayment();
+        transactionList.add(userPayment);
+
+        // Write into the file
+        logTransaction(userPayment);
+    }
+    static Transaction promptPayment(){
+
+        // Format for date / time
+        DateTimeFormatter logDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter logTimeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        // Fetch current date time and prompt user for deposit details
+        ZonedDateTime depositTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
+
+        println("What is the reason for the payment?");
+        String description = input.nextLine().trim();
+        println("Which Dokkaebi is receiving your payment?");
+        String vendor = input.nextLine().trim();
+        println("How many coins would like to pay?");
+        double amount = input.nextDouble();
+        input.nextLine(); // Clears buffer
+
+        Transaction userPayment = new Transaction(depositTime.format(logDateFormat), depositTime.format(logTimeFormat), description, vendor, -amount);
+        return userPayment;
+    }
+    static void logTransaction(Transaction userDeposit){
+
+        try{
+            FileWriter fileWriter = new FileWriter("star_stream_transactions.csv", true);
+            BufferedWriter bufWrite = new BufferedWriter(fileWriter);
+
+            bufWrite.write(userDeposit.getDate() +"|" +userDeposit.getTime() + "|" + userDeposit.getDescription()
+                            + "|" + userDeposit.getVendor() +"|" + userDeposit.getAmount() +"\n");
+            bufWrite.close();
+
+        } catch (Exception e) {
+            println("Could not write. File not found");
+        }
+    }
+
     static void displayLedgerMenu(){
         println("""
                 ------------------------------------------------------------------------------------------
@@ -102,33 +196,34 @@ public class AccountingApp {
                                                    (P)    Show Payments
                                                    (R)  Generate Reports
                                                    (H)    Back to Home
-                                                  
+                
                 ------------------------------------------------------------------------------------------""");
     }
     static void showAllTransaction(){
-        System.out.printf("%-10s | %-10s | %-30s | %-35s | %s", "Date",
+        System.out.printf("%-10s | %-10s | %-35s | %-35s | %s", "Date",
                 "Time", "Description", "Vendor", "Amount\n");
-        for (Transaction data: transactionList)
-        System.out.printf("%-10s | %-10s | %-30s | %-35s | %.2f\n", data.getDate(),
-                data.getTime(), data.getDescription(), data.getVendor(), data.getAmount());
+        for (Transaction data: transactionList) {
+            System.out.printf("%-10s | %-10s | %-35s | %-35s | %.2f\n", data.getDate(),
+                    data.getTime(), data.getDescription(), data.getVendor(), data.getAmount());
+        }
     }
     static void showDeposits(){
 
-        System.out.printf("%-10s | %-10s | %-30s | %-35s | %s", "Date",
+        System.out.printf("%-10s | %-10s | %-35s | %-35s | %s", "Date",
                 "Time", "Description", "Vendor", "Amount\n");
         for (Transaction data : transactionList){
             if (data.getAmount() > 0){
-                System.out.printf("%-10s | %-10s | %-30s | %-35s | %.2f\n", data.getDate(),
+                System.out.printf("%-10s | %-10s | %-35s | %-35s | %.2f\n", data.getDate(),
                         data.getTime(), data.getDescription(), data.getVendor(), data.getAmount());
             }
         }
     }
     static void showPayments(){
-        System.out.printf("%-10s | %-10s | %-30s | %-35s | %s", "Date",
+        System.out.printf("%-10s | %-10s | %-35s | %-35s | %s", "Date",
                 "Time", "Description", "Vendor", "Amount\n");
         for (Transaction data : transactionList){
             if (data.getAmount() < 0){
-                System.out.printf("%-10s | %-10s | %-30s | %-35s | %.2f\n", data.getDate(),
+                System.out.printf("%-10s | %-10s | %-35s | %-35s | %.2f\n", data.getDate(),
                         data.getTime(), data.getDescription(), data.getVendor(), data.getAmount());
             }
         }
