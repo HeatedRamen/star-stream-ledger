5be603047e0277d96dca87bb230c4e86ae2d49a4
ramenmac <kevinnguyen4200@gmail.com>
2026-04-27 18:21:08 -0500

    Add comments for code clarity

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -4,11 +4,14 @@ package com.pluralsight;
 import java.time.LocalDateTime;
 
 public class Transaction {
+
+    // Data from .csv, use DateTime for easier / "more precise" sorting
     private LocalDateTime dateTime;
     private String description;
     private String vendor;
     private long coins;
 
+    // Constructor
     Transaction(LocalDateTime currentDateTime, String description, String vendor, long price) {
         this.dateTime = currentDateTime;
         this.description = description;
@@ -16,18 +19,14 @@ public class Transaction {
         this.coins = price;
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
+    // Basic return functions
+    public LocalDateTime getDateTime() { return dateTime; }
     public String getDescription() {
         return description;
     }
-
     public String getVendor() {
         return vendor;
     }
-
     public long getAmount() {
         return coins;
     }
