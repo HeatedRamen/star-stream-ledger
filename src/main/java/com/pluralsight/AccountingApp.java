ef3fe84f529dd95ac250ab53de4b075fa6c41a5c
ramenmac <kevinnguyen4200@gmail.com>
2026-04-27 18:20:29 -0500

    Add comments for code clarity

--- src/main/java/com/pluralsight/AccountingApp.java
+++ src/main/java/com/pluralsight/AccountingApp.java
@@ -14,11 +14,13 @@ public class AccountingApp {
     static Scanner input = new Scanner(System.in);
     static ArrayList<Transaction> transactionList = new ArrayList<>();
 
+    static DateTimeFormatter logDateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     static DateTimeFormatter logDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     static DateTimeFormatter logTimeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     public static void main(String[] args) {
 
+        // Initialize variables for menu loops and choices
         boolean isRunning = true, inLedger, inReport;
         String userChoice, ledgerChoice, reportChoice;
 
@@ -40,15 +42,16 @@ public class AccountingApp {
                     makePayment();
                     break;
                 case "l":
-
-                    // Sorting Arraylist in descending / by newest
+                    // Sorting Arraylist in descending / by newest before any displays
                     sortTransactions();
-                    inLedger = true;
+                    inLedger = true; // Always start as true beforehand in case the user opens ledger multiple times
 
+                    // Displays Ledger menu and prompt user for ledger selection and continuous loop until user command exits
                     while (inLedger) {
                         displayLedgerMenu();
                         ledgerChoice = input.nextLine().toLowerCase();
 
+                        // Switch case for the ledger options and invalid input for default
                         switch (ledgerChoice) {
                             case "a":
                                 showAllTransaction();
@@ -60,12 +63,14 @@ public class AccountingApp {
                                 showPayments();
                                 break;
                             case "r":
-                                inReport = true;
+                                inReport = true; // Always start as true beforehand in case the user opens reports multiple times
 
+                                // Displays Ledger menu and prompt user for ledger selection and continuous loop until user command exits
                                 while (inReport) {
                                     displayReportMenu();
                                     reportChoice = input.nextLine();
 
+                                    // Switch case for report options and invalid input for default
                                     switch (reportChoice) {
                                         case "1":
                                             displayMonthToDate();
@@ -113,9 +118,6 @@ public class AccountingApp {
 
     static void loadData() {
 
-        // Format so String to DateTime can read
-        DateTimeFormatter logDateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
         try {
             // Opens and reads files
             FileReader fileReader = new FileReader("star_stream_transactions.csv");
@@ -127,17 +129,22 @@ public class AccountingApp {
             while (line != null) {
                 // Split the string and add it into transactions arraylist
                 parsedData = line.split("\\|");
+
+                // Convert String into LocalDateTime
                 LocalDateTime dateTime = LocalDateTime.parse(parsedData[0] + " " + parsedData[1], logDateTimeFormat);
+
+                // Add Transaction into Array list
                 transactionList.add(new Transaction(dateTime, parsedData[2], parsedData[3], Long.parseLong(parsedData[4])));
-                line = bufRead.readLine();
+                line = bufRead.readLine(); // Next line
             }
-        } catch (Exception e) {
+        }
+        // Catch case in case file is not found
+        catch (Exception e) {
             println("File could not be found");
         }
     }
 
     static void displayHomeMenu() {
-
         // Basic menu for now
         println("""
                 ------------------------------------------------------------------------------------------
@@ -163,9 +170,10 @@ public class AccountingApp {
 
     static Transaction promptDeposit() {
 
-        // Fetch current date time and prompt user for deposit details
+        // Fetch current date time
         ZonedDateTime depositTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
 
+        // Prompt user for information
         println("What is the reason for the deposit?");
         String description = input.nextLine().trim();
         println("What is your constellation modifier?");
@@ -174,8 +182,8 @@ public class AccountingApp {
         long amount = input.nextLong();
         input.nextLine(); // Clears buffer
 
+        // Return deposit information to be added into ArrayList and written into file
          return new Transaction(depositTime.toLocalDateTime(), description, vendor, amount);
-
     }
 
     static void makePayment() {
@@ -193,6 +201,7 @@ public class AccountingApp {
         // Fetch current date time and prompt user for deposit details
         ZonedDateTime depositTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
 
+        // Prompt user for information
         println("What is the reason for the payment?");
         String description = input.nextLine().trim();
         println("Which Dokkaebi is receiving your payment?");
@@ -201,26 +210,32 @@ public class AccountingApp {
         long amount = input.nextLong();
         input.nextLine(); // Clears buffer
 
+        // Returns payment information to be added into ArrayList and written into file
         return new Transaction(depositTime.toLocalDateTime(), description, vendor, -amount);
-
     }
 
     static void logTransaction(Transaction userDeposit) {
 
         try {
+            // Opening Buffered Writer and making sure to append
             FileWriter fileWriter = new FileWriter("star_stream_transactions.csv", true);
             BufferedWriter bufWrite = new BufferedWriter(fileWriter);
 
+            // Writing to match the formatting of the .csv file
             bufWrite.write(userDeposit.getDateTime().format(logDateFormat) + "|" + userDeposit.getDateTime().format(logTimeFormat) + "|" +
                     userDeposit.getDescription() + "|" + userDeposit.getVendor() + "|" + userDeposit.getAmount() + "\n");
-            bufWrite.close();
+            bufWrite.close(); // Close write
 
-        } catch (Exception e) {
+        }
+        // Catch case in case file could not be found
+        catch (Exception e) {
             println("Could not write. File not found");
         }
     }
 
     static void displayLedgerMenu() {
+
+        // Basic print menu for now
         println("""
                 ------------------------------------------------------------------------------------------
                 
@@ -234,10 +249,17 @@ public class AccountingApp {
                 ------------------------------------------------------------------------------------------""");
     }
 
-    static void showAllTransaction() {
+    static void printHeader(){
 
+        // Prints formatted header
         System.out.printf("%-10s | %-10s | %-35s | %-35s | %s", "Date",
                 "Time", "Description", "Vendor", "Amount\n");
+    }
+
+    static void showAllTransaction() {
+
+        // Prints header and goes through ArrayList and prints information into a table like manner
+        printHeader();
         for (Transaction data : transactionList) {
             System.out.printf("%-10s | %-10s | %-35s | %-35s | %d\n", data.getDateTime().format(logDateFormat),
                     data.getDateTime().format(logTimeFormat), data.getDescription(), data.getVendor(), data.getAmount());
@@ -246,8 +268,8 @@ public class AccountingApp {
 
     static void showDeposits() {
 
-        System.out.printf("%-10s | %-10s | %-35s | %-35s | %s", "Date",
-                "Time", "Description", "Vendor", "Amount\n");
+        // Prints header and goes through ArrayList and prints out only deposits (Only positive amounts) into a table like manner
+        printHeader();
         for (Transaction data : transactionList) {
             if (data.getAmount() > 0) {
                 System.out.printf("%-10s | %-10s | %-35s | %-35s | %d\n", data.getDateTime().format(logDateFormat),
@@ -258,8 +280,8 @@ public class AccountingApp {
 
     static void showPayments() {
 
-        System.out.printf("%-10s | %-10s | %-35s | %-35s | %s", "Date",
-                "Time", "Description", "Vendor", "Amount\n");
+        // Prints header and goes through ArrayList and prints out only payments (Only negative amounts) into a table like manner
+        printHeader();
         for (Transaction data : transactionList) {
             if (data.getAmount() < 0) {
                 System.out.printf("%-10s | %-10s | %-35s | %-35s | %d\n", data.getDateTime().format(logDateFormat),
@@ -269,6 +291,8 @@ public class AccountingApp {
     }
 
     static void displayReportMenu() {
+
+        // Basic print menu for now
         println("""
                 ------------------------------------------------------------------------------------------
                 
@@ -285,27 +309,33 @@ public class AccountingApp {
     }
 
     static void sortTransactions() {
+
+        // Sort transaction list gets date and time, B goes first in order to get descending sort
         transactionList.sort((a, b) -> b.getDateTime().compareTo(a.getDateTime()));
     }
 
     static void displayMonthToDate() {
 
+        // Get current date time
         LocalDateTime timeNow = LocalDateTime.now();
 
+
         for (Transaction data : transactionList) {
+            // Make comparison if transaction data's month is equal to current month and year then print out matches
             if (timeNow.getMonth() == data.getDateTime().getMonth() && timeNow.getYear() == data.getDateTime().getYear()) {
                 System.out.printf("%-10s | %-10s | %-35s | %-35s | %d\n", data.getDateTime().format(logDateFormat),
                         data.getDateTime().format(logTimeFormat), data.getDescription(), data.getVendor(), data.getAmount());
             }
-
         }
     }
 
     static void displayPreviousMonth() {
 
+        // Get current date time but go back one month
         LocalDateTime timeNow = LocalDateTime.now().minusMonths(1);
 
         for (Transaction data : transactionList) {
+            // Make comparison if transaction data's month is equal to the previous month and current year then print out matches
             if (timeNow.getMonth() == data.getDateTime().getMonth() && timeNow.getYear() == data.getDateTime().getYear()) {
                 System.out.printf("%-10s | %-10s | %-35s | %-35s | %d\n", data.getDateTime().format(logDateFormat),
                         data.getDateTime().format(logTimeFormat), data.getDescription(), data.getVendor(), data.getAmount());
@@ -315,9 +345,11 @@ public class AccountingApp {
 
     static void displayYearToDate() {
 
+        // Get current date time
         LocalDateTime timeNow = LocalDateTime.now();
 
         for (Transaction data : transactionList) {
+            // Make comparison if transaction data's year is equal to current year then print out matches
             if (timeNow.getYear() == data.getDateTime().getYear()) {
                 System.out.printf("%-10s | %-10s | %-35s | %-35s | %d\n", data.getDateTime().format(logDateFormat),
                         data.getDateTime().format(logTimeFormat), data.getDescription(), data.getVendor(), data.getAmount());
@@ -327,9 +359,11 @@ public class AccountingApp {
 
     static void displayPreviousYear() {
 
+        // Get current date time but go back one year
         LocalDateTime timeNow = LocalDateTime.now().minusYears(1);
 
         for (Transaction data : transactionList) {
+            // Make comparison if transaction data's year is equal to previous year then print out matches
             if (timeNow.getYear() == data.getDateTime().getYear()) {
                 System.out.printf("%-10s | %-10s | %-35s | %-35s | %d\n", data.getDateTime().format(logDateFormat),
                         data.getDateTime().format(logTimeFormat), data.getDescription(), data.getVendor(), data.getAmount());
@@ -338,11 +372,13 @@ public class AccountingApp {
     }
 
     static void promptVendorSearch() {
-        //
+
+        // Prompt user for vendor
         println("What is the name of the vendor you're looking for");
         String userVendor = input.nextLine();
 
         for (Transaction data : transactionList) {
+            // Make comparison if transaction data's vendor is equal to user supplied vendor and print out matches
             if (userVendor.equalsIgnoreCase(data.getVendor())) {
                 System.out.printf("%-10s | %-10s | %-35s | %-35s | %d\n", data.getDateTime().format(logDateFormat),
                         data.getDateTime().format(logTimeFormat), data.getDescription(), data.getVendor(), data.getAmount());
@@ -351,10 +387,13 @@ public class AccountingApp {
     }
 
     static void promptCustomSearch() {
+
+        // Basic looking custom search menu
         println("""
                         ~~~~~~~~~~~~~~~CUSTOM SEARCH~~~~~~~~~~~~~~~
                                    Press return to skip
                 What is the start date for your search? (yyyy-MM-dd)""");
+        // Prompt user for search filters
         String userStartDate = input.nextLine().trim();
         println("What is the end date for your search? (yyyy-MM-dd)");
         String userEndDate = input.nextLine().trim();
@@ -367,26 +406,32 @@ public class AccountingApp {
         println("What is the maximum range for your transaction?");
         String userMaxAmount = input.nextLine().trim();
 
-        // Converting String into LocalDateTime for Comparison
-        LocalDateTime startDate = null, endDate = null;
-        if (!userStartDate.isEmpty()) {
-            startDate = LocalDateTime.parse(userStartDate, logDateFormat);
-        }
-        if (!userEndDate.isEmpty()) {
-            endDate = LocalDateTime.parse(userEndDate, logDateFormat);
-        }
-
+        // To convert String into their respective variable
         Integer minAmount = null, maxAmount = null;
-        if (!userMinAmount.isEmpty()) {
-            minAmount = Integer.parseInt(userMinAmount);
-        }
-        if (!userMaxAmount.isEmpty()) {
-            maxAmount = Integer.parseInt(userMaxAmount);
+        LocalDateTime startDate = null, endDate = null;
+
+        // Try catch statement in case the user doesn't enter in valid input :P
+        try {
+            // Always checks if the string isn't empty, if it is empty leave it as null
+            if (!userStartDate.isEmpty()) {
+                startDate = LocalDateTime.parse(userStartDate, logDateFormat);
+            }
+            if (!userEndDate.isEmpty()) {
+                endDate = LocalDateTime.parse(userEndDate, logDateFormat);
+            }
+            if (!userMinAmount.isEmpty()) {
+                minAmount = Integer.parseInt(userMinAmount);
+            }
+            if (!userMaxAmount.isEmpty()) {
+                maxAmount = Integer.parseInt(userMaxAmount);
+            }
+        } catch (Exception e){
+            println("Invalid input");
         }
 
         for (Transaction data : transactionList) {
 
-            // One HUGE if statement that checks if the user input is empty OR equal to the transaction data
+            // One HUGE if statement that checks if the user input is empty / null OR equal to the transaction data
             if ((startDate == null || (!data.getDateTime().isAfter(startDate))) &&
                     (endDate == null || (!data.getDateTime().isBefore(endDate))) &&
                     (userDescription.isEmpty() || (userDescription.equalsIgnoreCase(data.getDescription()))) &&
